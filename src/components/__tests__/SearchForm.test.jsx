1a6153ef7a908687dbabfbe44210d59f4e2d38a7
Vlad Bezden <vlad.bezden@gmail.com>
2017-03-26 15:31:52 -0400

    added SearchForm test using enzyme and sinon

--- src/components/__tests__/SearchForm.test.jsx
+++ src/components/__tests__/SearchForm.test.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
+import { stub } from 'sinon'
 
 import SearchForm from '../SearchForm'
 
 test('SearchForm renders correctly', () => {
     const component = renderer.create(
-        <SearchForm onSearch={() => {}} />
+        <SearchForm onSearch={() => { }} />
     )
     const tree = component.toJSON()
 
     expect(tree).toMatchSnapshot()
+})
+
+test('SearchForm calls onSearch with the contents of the input when the form is submitted', () => {
+    const onSearchStub = stub()
+
+    const component = mount(
+        <SearchForm onSearch={onSearchStub} />
+    )
+
+    component.find('input').simulate('change', { target: { value: 'Test' } })
+    component.find('button').get(0).click()
+
+    expect(onSearchStub.callCount).toBe(1)
+    expect(onSearchStub.calledWith('Test')).toBe(true)
 })
