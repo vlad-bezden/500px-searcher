9bee4b5e235550266c2a12a0e20e6069d0f38ad3
Vlad Bezden <vlad.bezden@gmail.com>
2017-03-26 11:27:55 -0400

    added webpack-dev-server and configuration for it

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,28 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
+  // Which file is the entry point to the application
+  entry: './src/index.jsx',
   // Which file types are in our project, and where they are located
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  // Where to output the final bundled code to
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+    sourceMapFilename: 'app.map.js'
+  },
+  devtool: '#source-map',
   module: {
     // How to process project files with loaders
     loaders: [
       // Process any .js or .jsx file with Babel
       {
         test: /\.jsx?$/,
+        exclude: /node_modules/,
         loaders: ['babel-loader']
       }
     ]
-  },
-  // Which file is the entry point to the application
-  entry: [
-    './src/index.jsx'
-  ],
-  // Where to output the final bundled code to
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist'),
   }
 };
