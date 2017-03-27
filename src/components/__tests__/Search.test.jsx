70a926ba785fcbaf8ab8e8b780ef77ef748bb24c
Vlad Bezden <vlad.bezden@gmail.com>
2017-03-27 07:35:13 -0400

    fixing unit tests

--- src/components/__tests__/Search.test.jsx
+++ src/components/__tests__/Search.test.jsx
@@ -4,40 +4,41 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import fetchMock from 'fetch-mock'
 
+import Search from '../Search'
 import SearchForm from '../SearchForm'
 
 import { MOCK_500PX_PHOTOS_RESPONSE } from '../../tests/mocks'
 
 test('Search renders correctly', () => {
-    const component = shallow(
-        <Search />
-    )
-    const tree = component.getNode()
+  const component = shallow(
+    <Search />
+  )
+  const tree = component.getNode()
 
-    expect(tree).toMatchSnapshot()
+  expect(tree).toMatchSnapshot()
 })
 
 test('Search searches correctly', (done) => {
-    fetchMock.get(`https://api.500px.com/v1/photos/search?
+  fetchMock.get(`https://api.500px.com/v1/photos/search?
 		consumer_key=lILSGooAAcT8UA91VwlYViFeiY3SuPoebq9BtRQ4&
 		image_size[]=3&
 		image_size[]=4&
 		term=test`,
         MOCK_500PX_PHOTOS_RESPONSE)
 
-    const component = shallow(
-        <Search />
-    )
+  const component = shallow(
+    <Search />
+  )
 
-    const searchForm = component.find(SearchForm)
-    searchForm.prop('onSearch')('test')
+  const searchForm = component.find(SearchForm)
+  searchForm.prop('onSearch')('test')
 
-    setTimeout(() => {
-        const tree = component.getNode()
-        expect(tree).toMatchSnapshot()
+  setTimeout(() => {
+    const tree = component.getNode()
+    expect(tree).toMatchSnapshot()
 
-        expect(fetchMock.calls.length).toBe(1)
+    expect(fetchMock.calls.length).toBe(1)
 
-        done()
-    }, 0)
+    done()
+  }, 0)
 })
--- src/components/__tests__/Thumbnail.test.jsx
+++ src/components/__tests__/Thumbnail.test.jsx
@@ -4,10 +4,10 @@ import renderer from 'react-test-renderer'
 import Thumbnail from '../Thumbnail'
 
 test('Thumbnail renders correctly', () => {
-    const component = renderer.create(
-        <Thumbnail src='http://test.com/test.jpg' />
-    )
-    const tree = component.toJSON()
+  const component = renderer.create(
+    <Thumbnail src='http://test.com/test.jpg' />
+  )
+  const tree = component.toJSON()
 
-    expect(tree).toMatchSnapshot()
+  expect(tree).toMatchSnapshot()
 })
