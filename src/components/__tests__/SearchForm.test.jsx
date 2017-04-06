0be2e49c3cd0bd302ee6c25e2332724847dd08a0
Vlad Bezden <vlad.bezden@gmail.com>
2017-04-06 05:54:40 -0400

    simplifying and cleaning test cases

--- src/components/__tests__/SearchForm.test.jsx
+++ src/components/__tests__/SearchForm.test.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
-import { mount } from 'enzyme'
-import { stub } from 'sinon'
+import { mount, shallow } from 'enzyme'
 
 import SearchForm from '../SearchForm'
 
 test('SearchForm renders correctly', () => {
-  const component = renderer.create(
-    <SearchForm onSearch={() => { }} />
+  const component = shallow(
+    <SearchForm onSearch={f => f} />
   )
 
-  const tree = component.toJSON()
+  const tree = component.getNode()
 
   expect(tree).toMatchSnapshot()
 })
 
 test('SearchForm calls onSearch with the contents of the input when the form is submitted', () => {
-  const onSearchStub = stub()
-
+  const onSearchStub = jest.fn()
   const component = mount(
     <SearchForm onSearch={onSearchStub} />
   )
@@ -25,6 +22,5 @@ test('SearchForm calls onSearch with the contents of the input when the form is 
   component.find('input').simulate('change', { target: { value: 'Test' } })
   component.find('button').get(0).click()
 
-  expect(onSearchStub.callCount).toBe(1)
-  expect(onSearchStub.calledWith('Test')).toBe(true)
+  expect(onSearchStub).toBeCalledWith('Test')
 })
