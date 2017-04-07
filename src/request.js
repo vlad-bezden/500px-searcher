1a5a4bc4280b6e05acd086584c8fe77be771ea74
Vlad Bezden <vlad.bezden@gmail.com>
2017-04-06 20:53:53 -0400

    modified request method to be more es2015 style

--- src/request.js
+++ src/request.js
@@ -1,11 +1,9 @@
-export default function request(query) {
-  const apiUrl = `https://api.500px.com/v1/photos/search?
+const apiUrl = query => `https://api.500px.com/v1/photos/search?
 		consumer_key=lILSGooAAcT8UA91VwlYViFeiY3SuPoebq9BtRQ4&
 		image_size[]=3&
 		image_size[]=4&
 		term=${query}`
 
-  return fetch(apiUrl)
-    .then(response => response.json())
-    .catch(error => console.error(error))
-}
+export default query => fetch(apiUrl(query))
+  .then(response => response.json())
+  .catch(error => console.error(error))
