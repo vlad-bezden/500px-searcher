ce9a37649e75b0295f56bd02f0b53dd67963a978
Vlad Bezden <vlad.bezden@gmail.com>
2017-04-09 02:31:26 -0400

    updating linter and optimizing build

--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,17 @@ module.exports = {
         loaders: ['babel-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      warnings: false,
+      mangle: true
+    })
+  ]
 }
