98537affe2a8d5d9f454c1e2c92e8258df2735cd
Vlad Bezden <vlad.bezden@gmail.com>
2017-04-09 03:46:23 -0400

    moving bootstrap dependencies to bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,18 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loaders: ['babel-loader']
-      }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }       
     ]
   },
   plugins: [
